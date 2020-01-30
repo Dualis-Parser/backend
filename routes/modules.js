fac3fd24baa3e4b3706fda8ba93fcc02943a400b
Leonhard Gahr <leonhard.gahr@siemens.com>
2020-01-30 15:06:03 +0100

    semester filter is now stored in session; cache is now handled by this backend;

--- routes/modules.js
+++ routes/modules.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const request = require("request");
 const router = express.Router();
+const moment = require('moment');
 
 router.get('/', function (req, res, next) {
     if (!(req.session.loggedIn && req.session.username && req.session.password)) {
         res.sendStatus(401);
         return;
     }
-    console.log("Auth");
-    request.get(`https://api.gahr.dev/dualis/user?username=${encodeURIComponent(req.session.username)}&password=${encodeURIComponent(req.session.password)}`, function (err, resp, body) {
-        if (err) {
-            res.status(err.statusCode || 503).json({data: false});
-            return;
-        }
-        res.status(resp.statusCode).json(JSON.parse(body));
-    });
 
+    req.session.semesterFilter = JSON.parse(req.query.semesterFilter) || req.session.semesterFilter;
+
+    if (!req.session.moduleData || !req.session.lastUpdate || moment().subtract(2, 'minutes').isSameOrAfter(req.session.lastUpdate)) {
+        console.log("Updating");
+        req.session.lastUpdate = moment();
+
+        request.get(`https://api.gahr.dev/dualis/user?username=${encodeURIComponent(req.session.username)}&password=${encodeURIComponent(req.session.password)}`, function (err, resp, body) {
+            if (err) {
+                res.status(err.statusCode || 503).json({data: false});
+                return;
+            }
+            req.session.moduleData = JSON.parse(body);
+            req.session.moduleData.data.cache = 0;
+            req.session.moduleData.data.semesterFilter = req.session.semesterFilter;
+            res.status(resp.statusCode).json(req.session.moduleData);
+        });
+    } else {
+        req.session.moduleData.data.cache = Math.round(moment.duration(moment().diff(req.session.lastUpdate)).asSeconds());
+        req.session.moduleData.data.semesterFilter = req.session.semesterFilter;
+        res.json(req.session.moduleData);
+    }
 });
 
 module.exports = router;
