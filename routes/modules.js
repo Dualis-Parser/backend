aec19f6389adc6cecab71dc4d8118ecb08208c3b
Leonhard Gahr <leonhard.gahr@siemens.com>
2020-02-11 16:08:48 +0100

    feat(semester-sort): Semesterlist is now properly sorted and categorized by year feat(filter-update): Semesterfilter now has an own update route, reload of all data not required on filter change anymore fix(xlsx-export-year): Year now present in xlsx export feature(xlsx-export-structure): xslx file now has a blank line categorizing each study year

--- routes/modules.js
+++ routes/modules.js
@@ -5,13 +5,36 @@ const XLSX = require('xlsx');
 const router = express.Router();
 const moment = require('moment');
 
+const sortSemesters = semesters => {
+    const convToFullYear = (years) => {
+        years[1] = years[0].slice(0, 2) + years[1];
+        return years;
+    };
+    const zip = (...rows) => [...rows[0]].map((_, c) => rows.map(row => row[c]));
+
+    const studyYears = semesters
+        .map(semester => semester.split(' ')[1].split('/'))
+        .map(years => years.length === 2 ? convToFullYear(years) : years)
+        .map(years => years.map(year => +year));
+
+    return zip(semesters, studyYears)
+        // sort semesters descending
+        .sort((a, b) => a[1][0] < b[1][0] ? 1 : a[1][0] > b[1][0] ? -1 : a[1].length === 2 ? -1 : b[1].length === 2 ? 1 : 0)
+        .map(semester => semester[0]) // remove semester integer years
+        // create pair of two arrays to have one year in one array
+        .reduce((all, one, i) => {
+            const ch = Math.floor(i / 2);
+            all[ch] = [].concat((all[ch] || []), one);
+            return all;
+        }, []);
+};
+
 router.get('/', function (req, res) {
     if (!(req.session.loggedIn && req.session.username && req.session.password)) {
         res.sendStatus(401);
         return;
     }
 
-    req.session.semesterFilter = (req.query.semesterFilter && req.query.semesterFilter !== "null") ? JSON.parse(req.query.semesterFilter) : req.session.semesterFilter;
 
     if (!req.session.moduleData || !req.session.lastUpdate || moment().subtract(2, 'minutes').isSameOrAfter(req.session.lastUpdate)) {
         req.session.lastUpdate = moment();
@@ -35,6 +58,9 @@ router.get('/', function (req, res) {
             }
             req.session.moduleData.data.cache = Math.round(moment.duration(moment().diff(req.session.lastUpdate)).asSeconds());
             req.session.moduleData.data.semesterFilter = req.session.semesterFilter;
+
+            req.session.moduleData.data.semesters = sortSemesters(req.session.moduleData.data.semesters);
+
             res.status(resp.statusCode).json(req.session.moduleData);
         });
     } else {
@@ -50,8 +76,31 @@ router.get('/export', (req, res) => {
         return;
     }
 
-    let modules = req.session.moduleData.data.modules.map(module => [module.module_no, module.module_name, "", parseInt(module.credits), module.final_grade]);
-    modules = modules.filter(m => !m[4].match(/(not set yet)|(noch nicht gesetzt)/)).sort((a, b) => a[0] > b [0] ? 1 : -1); // sort, same module number is not possible
+    const getYearFromSemesters = (toSearch) => {
+        let year = NaN;
+        const semesters = req.session.moduleData.data.semesters;
+        semesters.forEach((yearSemesters, i) => yearSemesters.includes(toSearch.split(',')[0]) ? year = semesters.length - i : year);
+
+        return year;
+    };
+
+    let modules = req.session.moduleData.data.modules.map(module => [module.module_no, module.module_name, getYearFromSemesters(module.semesters), parseInt(module.credits), module.final_grade]);
+    modules = modules.filter(m => !m[4].match(/(not set yet)|(noch nicht gesetzt)/))
+        .sort((a, b) =>
+            a[2] - b[2] || a[0].localeCompare(b[0])
+        ); // sort, primary year, secondary module_no
+
+    // insert empty row after year change
+    let indicesToAdd = [];
+    let lastYear = 1;
+    modules.forEach((row, i) => {
+        if (row[2] !== lastYear) {
+            lastYear = row[2];
+            indicesToAdd.push(i);
+        }
+    });
+
+    indicesToAdd.forEach((add, i) => modules.splice(add + i, 0, [""])); // addition if i necessary, we add an element so the actual index moves);
     modules = [["Modul", "Fach", "Studienjahr", "Credits", "Note"], ...modules];
 
     const ws = XLSX.utils.aoa_to_sheet(modules);
@@ -65,6 +114,16 @@ router.get('/export', (req, res) => {
     res.setHeader('Content-disposition', `attachment; filename=modules.xlsx`);
     res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     res.send(buf);
+});
+
+router.post("/filter", (req, res) => {
+    if (!(req.session.loggedIn && req.session.username && req.session.password)) {
+        res.sendStatus(401);
+        return;
+    }
+
+    req.session.semesterFilter = req.body;
+    res.json({code:200, description: "OK"});
 });
 
 module.exports = router;
